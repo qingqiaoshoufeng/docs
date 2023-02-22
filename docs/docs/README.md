bdf1dc2e7bbf958b5fbcb6045cf9a13155a986f5
ouyangshun <ouyangshun@hxdi.com>
2023-02-22 18:44:41 +0800

    feat: 添加周会记录员

--- docs/docs/README.md
+++ docs/docs/README.md
@@ -1,19 +1,21 @@
 # 前端团队相关文档
 
+### 周会记录员
+<demo src="./components/who-is-meeting-registrar.vue"></demo>
 ### 开发计划
 [业务组件 & Utils工具集开发计划列表](https://docs.qq.com/sheet/DZll3ZXpBRUFCZFFY)
 
-[前端工程化工作计划和安排](https://docs.qq.com/doc/DZnFrTXJqS1R5bGpz)
+[前端工程化工作计划和安排](https://docs.qq.com/doc/DZnFrTXJqS1R5bGpz) <Badge type="warning" text="周更新"/>
 
 ### 项目
 
-[四所前端项目分布](https://docs.qq.com/sheet/DZkVXUmdmYkloU3Jj)
+[四所前端项目分布](https://docs.qq.com/sheet/DZkVXUmdmYkloU3Jj) <Badge text="月更新"/>
 
 ### 工作报告
 
 [四所周会会议纪要](https://docs.qq.com/doc/DZmNJbmRnRGR4bUNI)
 
-[前端周报集合](https://docs.qq.com/s/QBdM1-AOCDoR_jeRpmd6Eq)
+[前端周报集合](https://docs.qq.com/s/QBdM1-AOCDoR_jeRpmd6Eq) <Badge type="warning" text="周更新"/>
 
 ### 前端分享PPT
 
@@ -26,3 +28,4 @@
 ### 其他
 
 [前端紧急联系表](https://docs.qq.com/sheet/DZnNXeWR1aEp1bWJB?tab=BB08J2)
+
---
+++ docs/docs/components/who-is-meeting-registrar.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>本周周会记录员（需完成下列文档为 <strong>周更新</strong> 的文档内容）：</p>
+  <h1>{{ registrar().name }} 👨🏻‍💻 </h1>
+</template>
+
+<script setup>
+const getWeekOfYear = () => {
+  var currentDate = new Date()
+  var year = new Date(currentDate.getFullYear(), 0, 1)
+  var days = Math.floor((currentDate - year) / (24 * 60 * 60 * 1000))
+  var week = Math.ceil((currentDate.getDay() + 1 + days) / 7)
+  return week
+}
+
+// 排名不分先后
+const registrars = [
+  { name: '欧阳顺', status: true },
+  { name: '卯升获', status: true },
+  { name: '毕丽静', status: true },
+  { name: '郑书彬', status: true },
+  { name: '杨川', status: true },
+  { name: '和肖飞', status: true },
+  { name: '洪静怡', status: true },
+  { name: '刘楷', status: true },
+  { name: '金凯', status: true },
+  { name: '董建麟', status: true },
+  { name: '付明', status: true },
+]
+
+const registrar = () => {
+  const registrarNum = registrars.length
+  const index = (getWeekOfYear() + registrarNum) % registrarNum
+  return registrars.filter((i) => i.status)[index]
+}
+</script>
+
+<style lang="scss" scoped></style>
