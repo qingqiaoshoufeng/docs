2b6a0d9b4a394fdf7a17b21f58448908ef40d5f9
jinkai <jinkai@hxdi.com>
2023-03-02 08:57:19 +0800

    feat: 更新数字组件文档，增加方法

--- docs/components/data-visualization/virtual-number-demos/demo5.vue
+++ docs/components/data-visualization/virtual-number-demos/demo5.vue
@@ -2,17 +2,36 @@
 import { ref } from 'vue'
 
 const num = ref(12345)
-const onClick = () => {
-  num.value = Math.random() * 10000
+const counter = ref(null)
+const start = () => {
+  counter.value.start()
+}
+const reset = () => {
+  counter.value.reset()
+}
+const pause = () => {
+  counter.value.pauseResume()
+}
+const restart = () => {
+  counter.value.restart()
 }
 </script>
 
 <template>
   <div class="demo5">
     <ScaleScreen :fit="true" :fullscreen="false">
-      <VirtualNumber class="demo-number" :value="num" count-up />
-      <a-button type="primary" @click="onClick">
-        计数
+      <VirtualNumber ref="counter" class="demo-number" :value="num" count-up />
+      <a-button type="primary" style="margin-right: 10px" @click="reset">
+        重置
+      </a-button>
+      <a-button type="primary" style="margin-right: 10px" @click="pause">
+        暂停
+      </a-button>
+      <a-button type="primary" style="margin-right: 10px" @click="start">
+        开始
+      </a-button>
+      <a-button type="primary" @click="restart">
+        重启
       </a-button>
     </ScaleScreen>
   </div>
