83d8c614acf08d42488900ab2423dd0622446a06
ouyangshun <ouyangshun@hxdi.com>
2024-03-18 14:30:24 +0800

    feat: 更新文档

--- docs/.vuepress/components/package-version.vue
+++ docs/.vuepress/components/package-version.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <img :src="`https://img.shields.io/badge/${name}-Latest:v${info?.latest?.version ?? '?.?.?'}-blue`" />
+    <!-- <img :src="`https://img.shields.io/badge/${name}-Latest:v${info?.latest?.version ?? '?.?.?'}-blue`" /> -->
+    <img :src="`https://badgen.net//badge/Latest/${info?.latest?.version ? 'v' + info?.latest?.version : 'unknown'}`" />
   </div>
 </template>
 
